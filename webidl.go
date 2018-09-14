bd3dce6bfc5cab468e956e7eca1b2b991f5a252b
gernest <geofreyernest@live.com>
2018-09-14 13:54:17 +0300

    complete parsing extended attributes form

--- webidl.go
+++ webidl.go
@@ -52,10 +52,12 @@ type Parser struct {
 type State uint
 
 const (
-	OpenInterface = 1 << iota
+	OpenInterface State = 1 << iota
 	EndInterface
 	OpenExtendedAttribute
 	EndExtendedAttribute
+	OpenArgs
+	EndArgs
 )
 
 func (p *Parser) Parse(fset *token.FileSet, filename string, src []byte) error {
@@ -105,6 +107,15 @@ func (p *Parser) peek() *Token {
 	return x
 }
 
+func delimAttribute(tok token.Token) bool {
+	switch tok {
+	case token.COMMA, token.RBRACK:
+		return true
+	default:
+		return false
+	}
+}
+
 func (p *Parser) parseAttribute() {
 	x := p.next()
 	switch x.Tok {
@@ -113,12 +124,131 @@ func (p *Parser) parseAttribute() {
 		switch next.Tok {
 		case token.COMMA, token.RBRACK:
 			n := ExtendedAttributeNoArgs{
-				Indent: x.Text,
+				Ident: x.Text,
 			}
 			n.pos = x.Pos
 			n.end = next.Pos
 			if v, ok := p.current.(*ExtendedAttributeList); ok {
 				v.List = append(v.List, n)
+			}
+		case token.ASSIGN:
+			p.next() // consume =
+			next := p.next()
+			switch next.Tok {
+			case token.LPAREN:
+				// takes an identifier list
+				//
+				//	[Exposed=(Window,Worker)]
+				n := &ExtendedAttributeIdentList{}
+				n.Name = x.Text
+				n.pos = x.Pos
+				for {
+					tok := p.next()
+					switch tok.Tok {
+					case token.IDENT:
+						n.Idents = append(n.Idents, tok.Text)
+					case token.COMMA:
+						continue
+					case token.RPAREN:
+						if v, ok := p.current.(*ExtendedAttributeList); ok {
+							v.List = append(v.List, n)
+						}
+						return
+					default:
+						//TODO : return error
+					}
+				}
+			case token.IDENT:
+				peek := p.peek()
+				switch {
+				case delimAttribute(peek.Tok):
+					// takes an identifier
+					//
+					// [PutForwards=name]
+					n := ExtendedAttributeIdent{}
+					n.Name = x.Text
+					n.Ident = next.Text
+					n.end = peek.Pos
+					if v, ok := p.current.(*ExtendedAttributeList); ok {
+						v.List = append(v.List, n)
+					}
+					return
+				case peek.Tok == token.LPAREN:
+					p.next()
+					n := &ExtendedAttributeNamedArgList{}
+					n.Name = x.Text
+					n.Ident = next.Text
+					n.pos = x.Pos
+					state := OpenArgs
+					var arg []string
+					for {
+						next := p.next()
+						switch next.Tok {
+						case token.RPAREN:
+							if arg != nil {
+								n.Args = append(n.Args, arg)
+								arg = nil
+							}
+							state = EndArgs
+						case token.COMMA:
+							if arg != nil {
+								n.Args = append(n.Args, arg)
+								arg = nil
+							}
+							arg = nil
+							continue
+						case token.IDENT:
+							arg = append(arg, next.Text)
+						}
+						switch state {
+						case EndArgs:
+							peek := p.peek()
+							n.end = peek.Pos
+							if v, ok := p.current.(*ExtendedAttributeList); ok {
+								v.List = append(v.List, n)
+							}
+							return
+						}
+					}
+				}
+
+			}
+		case token.LPAREN:
+			p.next()
+			n := ExtendedAttributeArgList{
+				Ident: x.Text,
+			}
+			n.pos = x.Pos
+			state := OpenArgs
+			var arg []string
+			for {
+				next := p.next()
+				switch next.Tok {
+				case token.RPAREN:
+					if arg != nil {
+						n.Args = append(n.Args, arg)
+						arg = nil
+					}
+					state = EndArgs
+				case token.COMMA:
+					if arg != nil {
+						n.Args = append(n.Args, arg)
+						arg = nil
+					}
+					arg = nil
+					continue
+				case token.IDENT:
+					arg = append(arg, next.Text)
+				}
+				switch state {
+				case EndArgs:
+					peek := p.peek()
+					n.end = peek.Pos
+					if v, ok := p.current.(*ExtendedAttributeList); ok {
+						v.List = append(v.List, n)
+					}
+					return
+				}
 			}
 		}
 	}
@@ -143,7 +273,28 @@ type ExtendedAttributeList struct {
 
 type ExtendedAttributeNoArgs struct {
 	position
-	Indent string
+	Ident string
+}
+
+type ExtendedAttributeArgList struct {
+	position
+	Ident string
+	Args  [][]string
+}
+
+type ExtendedAttributeIdent struct {
+	ExtendedAttributeNoArgs
+	Name string
+}
+type ExtendedAttributeNamedArgList struct {
+	ExtendedAttributeArgList
+	Name string
+}
+
+type ExtendedAttributeIdentList struct {
+	position
+	Name   string
+	Idents []string
 }
 
 func (p *Parser) next() *Token {
